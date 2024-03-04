fa163f6225d6eec4aa7fa34ff1bf65dcee5cd9f8
Marica Romano <marica.romano@studio.unibo.it>
2024-03-04 12:15:32 +0100

    Complete first test class refactoring

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -7,60 +7,36 @@ public class LogicTest {
 
   private Logics logic;
 
+  @BeforeEach
+  public void createTableTest() {
+    this.logic = new LogicsImpl(SIZE_TABLE, new Pair<>(2,2), new Pair<>(4,1));
+  }
+
   @Test
   public void correctlyCreatedKnight() {
-    this.logic = new LogicsImpl(SIZE_TABLE);
-    //Test the creation of Knight, but not in the first cell
     boolean cellHasKnight = this.logic.hasKnight(1, 1);
     assertFalse(cellHasKnight);
   }
 
   @Test
   void correctlyCreatedPawn() {
-    this.logic = new LogicsImpl(SIZE_TABLE);
-    //Test the creation of Pawn, but not in the first cell
     boolean cellHasPawn = this.logic.hasPawn(1, 1);
     assertFalse(cellHasPawn);
   }
 
   @Test
   public void throwsExceptionWhenOutOfTable() {
-    this.logic = new LogicsImpl(SIZE_TABLE);
     assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(-1, -1));
   }
 
   @Test
   public void throwsExceptionOnTableBorder() {
-    this.logic = new LogicsImpl(SIZE_TABLE);
     assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(SIZE_TABLE, SIZE_TABLE));
   }
 
   @Test
   public void hitCorrectlyFunctioning() {
-    this.logic = new LogicsImpl(SIZE_TABLE, new Pair<>(2,2), new Pair<>(4,1));
     assertTrue(this.logic.hit(4,1));
   }
 
-  @Test
-  public void test() {
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-
-    this.logic = new LogicsImpl(SIZE_TABLE);
-    //Test the creation of Knight, but not in the first cell
-    boolean cellHasKnight = this.logic.hasKnight(1, 1);
-    assertFalse(cellHasKnight);
-    //Test the creation of Pawn, but not in the first cell
-    boolean cellHasPawn = this.logic.hasPawn(1, 1);
-    assertFalse(cellHasPawn);
-
-    //Test hit()'s exceptions
-    assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(-1, -1));
-    assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(SIZE_TABLE, SIZE_TABLE));
-
-    this.logic = new LogicsImpl(SIZE_TABLE, new Pair<>(2,2), new Pair<>(4,1));
-    assertTrue(this.logic.hit(4,1));
-  }
 }
